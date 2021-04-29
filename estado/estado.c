d78bc9279fa2591e5f54a72b22a45fcf7f5484f9
helenafr <hele0028@gmail.com>
2021-04-29 17:16:32 +0200

    commit estado 29/4 17:16

--- estado/estado.c
+++ estado/estado.c
@@ -14,11 +14,11 @@ void EstadosPersonas(ListaEnlazadaRef lista){
     int flag, indice_borrar;
     flag=0;
 
-    while(nodo_aux != NULL){
+    while(nodo_aux->sig != NULL){
 
         indice_borrar=ActualizarEstado(nodo_aux,&flag);
         if (flag == 1){
-//            eliminarNodoPosicionIndice(lista, indice_borrar);
+            eliminarNodoPosicionIndice(lista, indice_borrar);
         }
 
         nodo_aux = nodo_aux->sig;
@@ -42,6 +42,7 @@ int ActualizarEstado(tipoNodoRef nodo,int *flag){  //devuelve indice si la perso
 	if (persona->estado == 2){ //si la persona tiene covid, cuando se recupera
             if (persona->cont_recu == PERIODO_RECUPERACION){
                 persona->estado = 3;
+                PersonaSana(persona->id); //
                 persona->cont_recu = 0;
             }
             else{
@@ -64,20 +65,47 @@ int ActualizarEstado(tipoNodoRef nodo,int *flag){  //devuelve indice si la perso
 
 void VacunarPersonas(ListaEnlazadaRef lista){
     tipoNodoRef nodo_aux = *lista;
-    Persona persona;
     int vacunados;
 
+  //  vacunados = N_PERSONAS_VACU_T;
+    printf("gshgs");
+                fflush(stdin);
     vacunados = N_PERSONAS_VACU_T;
-
+    if(!estaVacia(*lista)){
     while((nodo_aux->sig != NULL) || (vacunados != 0)){
 
-        persona = (Persona) nodo_aux->info;
+//        Persona *persona = (Persona*) &nodo_aux->info;
 
-        if ((persona.estado == 0) || (persona.estado == 3)){
-            persona.estado = 4; //si que quiere aplicar politica, switch
+        if ((nodo_aux->info.estado == 0) || (nodo_aux->info.estado == 3)){
+            nodo_aux->info.estado = 4; //si que quiere aplicar politica, switch
         }
         vacunados--;
 
 	nodo_aux = nodo_aux->sig;
+    }
+    }
+}
+
+
+void PersonaSana(int indice){
+   tipoNodoRef nodo_sano = *sanos;
+   tipoNodoRef nodo_contagiado = *contagiados;
+   tipoNodoRef nodo_ant;
+
+    while(nodo_contagiado->sig != NULL){
+        Persona *persona = (Persona*) &nodo_contagiado->info;
+        if ( persona->id != indice){
+            nodo_ant = nodo_contagiado;
+            nodo_contagiado = nodo_contagiado->sig;
+        }
+        else{
+            printf("%d- %d",persona->id,indice);
+            while(nodo_sano->sig != NULL){
+                nodo_sano = nodo_sano->sig;
+            }
+            nodo_sano->sig = nodo_contagiado;
+            nodo_ant->sig = nodo_contagiado->sig;
+            nodo_contagiado->sig = NULL;
+        }
     }
 }
