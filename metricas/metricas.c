870d0d2fe13d5ea1ca4d559ad16ef3c5cb97e502
Aingeru Ramos <aingeru.ramos.auzmend@gmail.com>
2021-04-30 10:28:11 +0200

    Metricas con carpetas

--- metricas/metricas.c
+++ metricas/metricas.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 #include "../globales.h"
 #include "../listaEnlazadaSimple.h"
 
 void GuardarPosiciones(char* nombre) {
-    char* nombreFichero = (char*) malloc(sizeof(char)*14);
+    char* nombreFichero = (char*) malloc(sizeof(char)*200);
     strcpy(nombreFichero, nombre);
     strcat(nombreFichero, ".pos");
 
@@ -40,7 +41,7 @@ void GuardarPosiciones(char* nombre) {
 }
 
 void GuardarMetricas(char* nombre) {
-    char* nombreFichero = (char*) malloc(sizeof(char)*19);
+    char* nombreFichero = (char*) malloc(sizeof(char)*200);
     strcpy(nombreFichero, nombre);
     strcat(nombreFichero, ".metricas");
 
@@ -59,26 +60,23 @@ void GuardarMetricas(char* nombre) {
     fprintf(fp, "RECUPERADOS: %f\n", M_RECUPERADOS);
     fprintf(fp, "FALLECIDOS: %f\n", M_FALLECIDOS);
     fprintf(fp, "R0: %f\n", R0);
-    
+
     M_SANOS = 0.0;
     M_CONTAGIADOS = 0.0;
     M_RECUPERADOS = 0.0;
     M_FALLECIDOS = 0.0;
     R0 = 0.0; //Esto probablemente no sea asi
-  
+
     fclose(fp);
     free(nombreFichero);
 }
 
 void GuardarDatos(int id_metricas) {
-    char* ruta = (char*) malloc(sizeof(char)*16);
-    strcpy(ruta, "datos/");
+    char* ruta = (char*) malloc(sizeof(char)*200);
+    sprintf(ruta, "datos/%d_%d_%d_%d_%.2f_%.2f_%.2f/", N_PERSONAS, MAX_X, MAX_Y, TIEMPO_SIMULACION, ALFA, BETA, PORCENT_VACUNACION);
+    mkdir(ruta, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
-    char* nombre = (char*) malloc(sizeof(char)*10);
-    sprintf(nombre, "%d", id_metricas);
-
-    strcat(ruta, nombre);
-    free(nombre);
+    sprintf(ruta, "%s%d", ruta, id_metricas);
 
     GuardarPosiciones(ruta);
     GuardarMetricas(ruta);
